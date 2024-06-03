4da458c07bca2f8af9fc6649c420c92237c48337
dm.choi <dm.choi.dev@gmail.com>
2024-06-03 20:41:11 +0900

    update: reissue, logout api route 추가, fix: login api http -> axios 변경

--- app/api/reissue/route.ts
+++ app/api/reissue/route.ts
@@ -1,33 +1,34 @@
-import { NextApiResponse } from 'next';
-
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
+
+import * as https from 'https';
 
 import axios from 'axios';
 
+import { baseURL } from '~/constants/url';
 import { ICommonResponse } from '~/types/apis/common.types';
 import { ILoginResponse } from '~/types/apis/user.types';
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function POST(req: NextRequest, res: NextApiResponse) {
-  const requestBody = await req.json();
+export async function POST(req: NextRequest, res: NextResponse) {
   try {
+    const refreshToken = cookies().get('refresh_token')?.value;
     const response = await axios.post<ICommonResponse<ILoginResponse>>(
-      '/users/login',
-      {
-        username: requestBody.username,
-        password: requestBody.password,
-      },
+      '/users/reissue',
+      {},
       {
         baseURL,
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
       },
     );
-
-    const { accessToken, refreshToken } = response.data.data;
-
+    const { accessToken, refreshToken: newRefreshToken } = response.data.data;
     const newHeaders = new Headers();
     newHeaders.set('set-cookie', `access_token=${accessToken}; path=/; samesite=Lax; secure=true;`);
     newHeaders.append('set-cookie', `refresh_token=${refreshToken}; path=/; samesite=Lax; httponly; secure=true;`);
-
     return NextResponse.json({ ...response.data.data }, { status: response.status, headers: newHeaders });
   } catch (error: any) {
     return NextResponse.json({ ...error }, { status: error.status });
